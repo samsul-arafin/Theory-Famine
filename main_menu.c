84a78581855c0e8babc6f51709b97dd76c4ffcb8
Samsul Arafin <samsularafin05@gmail.com>
2025-12-11 20:32:50 +0600

    just added simple ascii

---
+++ main_menu.c
@@ -0,0 +1,37 @@
+
+int title(){
+    //Sleep(10000); // Pause for 10 seconds to view the title screen
+    //system("cls"); // Clear the console screen
+
+    printf(BOLD_RED "                                                                                                                                                                \n" RESET);
+    printf(BOLD_RED "                                                                                                                                                                \n" RESET);
+    printf(BOLD_RED "                                                                                                                                                                \n" RESET);
+    printf(BOLD_RED "   ########  ##    ##  ########            #######   ########  ##    ##   \n" RESET);
+    printf(BOLD_RED "   ########  ##    ##  ########            #######   ########  ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ##                  ##           ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ##                  ##           ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ##                  ##### .      ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ########  #######             #######.     ##     ########   \n" RESET);
+    printf(BOLD_RED "      ##     ########  #######             #:  .###     ##     ########   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ##                        ##     ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ##                        ##     ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ##                  #:  .###     ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ########            #######.     ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "      ##     ##    ##  ########            :#### .      ##     ##    ##   \n" RESET);
+    printf(BOLD_RED "                                                                                                                                                                \n" RESET);
+    printf(BOLD_RED "                                                                                                                                                                \n" RESET);
+    printf(BOLD_RED "                                                                                                                                                                \n" RESET);
+    printf(BOLD_RED "   ##    ##   .####.   ######:    :####:   ########  ###  ###    :##:    ###   ## \n" RESET);
+    printf(BOLD_RED "   ##    ##   ######   #######   :######   ########  ###  ###     ##     ###   ## \n" RESET);
+    printf(BOLD_RED "   ##    ##  :##  ##:  ##   :##  ##:  :#   ##        ###::###    ####    ###:  ## \n" RESET);
+    printf(BOLD_RED "   ##    ##  ##:  :##  ##    ##  ##        ##        ###  ###    ####    ####  ## \n" RESET);
+    printf(BOLD_RED "   ##    ##  ##    ##  ##   :##  ###:      ##        ## ## ##   :#  #:   ##:#: ## \n" RESET);
+    printf(BOLD_RED "   ########  ##    ##  #######:  :#####:   #######   ##:##:##    #::#    ## ## ## \n" RESET);
+    printf(BOLD_RED "   ########  ##    ##  ######     .#####:  #######   ##.##.##   ##  ##   ## ## ## \n" RESET);
+    printf(BOLD_RED "   ##    ##  ##    ##  ##   ##.      :###  ##        ## ## ##   ######   ## :#:## \n" RESET);
+    printf(BOLD_RED "   ##    ##  ##:  :##  ##   ##         ##  ##        ##    ##  .######.  ##  #### \n" RESET);
+    printf(BOLD_RED "   ##    ##  :##  ##:  ##   :##  #:.  :##  ##        ##    ##  :##  ##:  ##  :### \n" RESET);
+    printf(BOLD_RED "   ##    ##   ######   ##    ##: #######:  ########  ##    ##  ###  ###  ##   ### \n" RESET);
+    printf(BOLD_RED "   ##    ##   .####.   ##    ### .#####:   ########  ##    ##  ##:  :##  ##   ### \n" RESET);
+    return 0;
+}
